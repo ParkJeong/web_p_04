188087593324c467eb3b1fd19a47f9e3e05e0f8a
박정훈 <kangtak6291@naver.com>
2020-05-12 21:01:56 +0900

    Design CSS

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+     # 7행에서 저장한 posts 변수의 값을
+     # html파일의 코드안에 있는 변수 posts에 대입한다는 의미
+    return render(request, 'blog/post_list.html', {'posts' : posts})
+   
