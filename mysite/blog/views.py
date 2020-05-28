ad56cc1167eb9fdcaf474d9706f5a7d608deed77
박정훈 <kangtak6291@naver.com>
2020-05-28 20:29:32 +0900

    expends application

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
@@ -9,3 +9,6 @@ def post_list(request):
      # html파일의 코드안에 있는 변수 posts에 대입한다는 의미
     return render(request, 'blog/post_list.html', {'posts' : posts})
    
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
